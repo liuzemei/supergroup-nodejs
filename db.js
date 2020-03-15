ed20365e2796f60dc08e1292cc8c65b0a17f25d2
liuzemei <her_liu@qq.com>
2020-03-15 14:43:26 +0800

    rename query

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const pgsql = require('pg')
 const { DATABASE_CONFIG } = require('./config')
 
 
-async function query(sql, params) {
+async function tx(sql, params) {
   const client = new pgsql.Client(DATABASE_CONFIG)
   await client.connect()
   const { rows } = await client.query(sql, params)
@@ -13,11 +13,11 @@ async function query(sql, params) {
 class DB {
   async add_user(user_id, conversation_id) {
     let sql = 'INSERT INTO users(user_id, conversation_id) VALUES($1::varchar, $2::varchar) ON CONFLICT(user_id) DO NOTHING'
-    return await query(sql, [user_id, conversation_id])
+    return await tx(sql, [user_id, conversation_id])
   }
   async get_user(user_id) {
     let sql = 'SELECT * FROM users WHERE user_id!=$1'
-    return await query(sql, [user_id])
+    return await tx(sql, [user_id])
   }
 }
 
--- message.js
+++ message.js
@@ -5,17 +5,17 @@ async function forward_message(client, message, representative_id) {
   let all_user = await db.get_user(representative_id)
   let ids = up_to_100_ids(all_user)
   for (let i = 0; i < ids.length; i++) {
-    let params = ids[i].map(({ conversation_id, user_id }) => get_message_params(client, conversation_id, user_id, message, representative_id))
+    let params = ids[i].map(({ conversation_id, user_id }) => get_message_params(client, message, conversation_id, user_id, representative_id))
     await client._request.post('/messages', params)
   }
   return true
 }
 
-function get_message_params(client, conversation_id, recipient_id, message, representative_id) {
-  let { category, data, quote_message_id } = message
+function get_message_params(client, message, conversation_id, recipient_id, representative_id) {
+  let { category, data } = message
   let message_id = client.getUUID()
   if (category === 'PLAIN_TEXT') data = Buffer.from(data.toString()).toString('base64')
-  return { conversation_id, recipient_id, message_id, category, data, representative_id, quote_message_id }
+  return { conversation_id, recipient_id, message_id, category, data, representative_id }
 }
 
 function up_to_100_ids(array) {
