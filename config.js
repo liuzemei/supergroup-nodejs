ece0704a0c022a7831a1a894928458a677516feb
liuzemei <her_liu@qq.com>
2020-03-17 19:13:44 +0800

    1

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const DATABASE_CONFIG = {
 
 const CLIENT_CONFIG = {
   app_number: "7000102578", // 1054912 没有实际使用，只是提醒自己是那个机器人。
-  client_id: "454e42f8-7d80-4759-9d80-05f641b201d4",
-  session_id: "3bcf8e4a-37dc-4e39-af19-c99c6077e2fb",
-  private_key: "-----BEGIN RSA PRIVATE KEY-----\r\nMIICXAIBAAKBgQCdnzwFLcqQHprsC6SNFkuDNhsXwy29KDdfGQSZMjwS9BjlS1+A\r\ndCHmYs4RkiKl1SeGt3JcdHioC63sQgnGO117EZtexBTeVAsu0Lf+B7W0n09Cw4sG\r\nuosR6dL0bbLSfgZNdfbsE2VCfZU8+jWRDDtm3RE3i42rw5hykqjqZ1evkQIDAQAB\r\nAoGAKSr2xS+BkYS819vunX7uOBYAm4M5xgbj922UWuLY9+ZTPkgiaG1skTzLL6Go\r\ndQGoMjJlpyowfUuXoHZbETBeKKWZZHzTmgEZuw+PGA5649hiNsb/d1RYWIlkspn5\r\n63aVHnX60Qz82BUxfAgwQozL4+81UuRf8YClAKPbUpnj2GUCQQDoCIHEUqIlo0/H\r\nd71UTT824bneZp3EYRuVhaysymZCfJ+kAgf0qc3HcviwugP0i8Wt57oYn+ukL+F/\r\nFEx/7nqbAkEArecfm/yUnrLT55U85LfqFvZVeTtY8lcOVmCSzp1aQ1YJH9e92dEr\r\nueTt5ElkmygeSe3jqXmt/MNYPwcoOlHbQwJAcLf5OkMeTAR69B6CFhr0k5eycI0P\r\nJWA7QzbN/J7ScIgUEVt/O+bldXevwkKLO1hPLr08lUvS8E7mT5Kv2xIh8wJBAKmQ\r\neNPCHq/3MoPWkn95jLlDC+ifo4T4ubQjikIIT+DkXRU9aZKfUAF8ZTF5Fq3rGk1/\r\ntcKkMLALUVpzghgLhL0CQDAs/42n1v4f+FbvSOuDkiT2pIEWz18+3Nx3xarzFHEQ\r\nkjQW2CeKIERAEsywN5Qa6MQC4bYuX3+hgzNxC+lpfDs=\r\n-----END RSA PRIVATE KEY-----\r\n"
+  client_id: "454e42f8-xxxx-xxxx-xxxx-05f641b201d4",
+  session_id: "3bcf8e4a-xxxx-xxxx-xxxx-c99c6077e2fb",
+  private_key: "-----BEGIN RSA PRIVATE KEY-----\r\nMIICXAIBAAKBgQCdnzwFLcqQHxxxx6SNFkuDxxxxwy29KDdfGQSZMjwS9BjlS1+A\r\ndCHmYs4RkiKl1xxxx3JcdHioC63sQgnGO117EZtexBTexxxx0Lf+B7W0n09Cw4sG\r\nuosR6dL0bbLSfgZNdfxxxxVCfZU8+jWRxxxxxxx............xxxxxxxfDs=\r\n-----END RSA PRIVATE KEY-----\r\n"
 }
 
 
